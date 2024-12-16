e7acbb47f74c377bac9f3ce5706f9b339e563e38
Abdur Rauf <raufabdur886@gmail.com>
2024-12-16 16:46:30 +0500

    `Update NotifierService to add clickable notification functionality`

--- projects/softo-ngx-components/src/lib/services/notifier/notifier.service.ts
+++ projects/softo-ngx-components/src/lib/services/notifier/notifier.service.ts
@@ -2,11 +2,10 @@ import { Injectable } from '@angular/core';
 import { NotifierService as AngularNotifierService } from 'angular-notifier-3';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class NotifierService {
-
-  constructor(private notifier: AngularNotifierService) { }
+  constructor(private notifier: AngularNotifierService) {}
 
   public error(message: string, notificationId?: string) {
     this.notifier.notify('error', message, notificationId);
@@ -14,6 +13,13 @@ export class NotifierService {
 
   public success(message: string, notificationId?: string) {
     this.notifier.notify('success', message, notificationId);
+  }
+
+  public clickable(message: string, link?: string, notificationId?: string) {
+    const formattedMessage = link
+      ? `${message} <a href="${link}" target="_blank">Visit Link</a> | <button class="copy-link-btn" onclick="navigator.clipboard.writeText('${link}')">Copy Link</button>`
+      : message;
+    this.notifier.notify('success', formattedMessage, notificationId);
   }
 
   public info(message: string, notificationId?: string) {
